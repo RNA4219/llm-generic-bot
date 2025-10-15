43f800449bcf1f72ffa5c4bf4f09fbc93a870c7a
R_N_A <33271130+RNA4219@users.noreply.github.com>
2025-10-15 09:24:59 +0900

    Fix orchestrator package exports

---
+++ src/llm_generic_bot/core/orchestrator/__init__.pyi
@@ -0,0 +1,25 @@
+from ..orchestrator import (
+    MetricsRecorder,
+    NullMetricsRecorder,
+    Orchestrator,
+    PermitDecision,
+    PermitDecisionLike,
+    PermitEvaluator,
+    Sender,
+    _SendRequest,
+    metrics_module,
+    processor,
+)
+
+__all__ = [
+    "MetricsRecorder",
+    "NullMetricsRecorder",
+    "Orchestrator",
+    "PermitDecision",
+    "PermitDecisionLike",
+    "PermitEvaluator",
+    "Sender",
+    "_SendRequest",
+    "metrics_module",
+    "processor",
+]
